cae3e22e4a18cc4e0ed20a1b7072338daa23f0a5
CopyPaste <sjnnj@naver.com>
2024-09-19 18:18:39 +0900

    #41 - `UserAccount` 생성자 추가 작업
    `UserAccountService` 에서 회원정보를 저장하는 로직을 작성하여서 `UserAccount` 에 생성자 오버라이드를 추가하였다.

--- src/main/java/com/community/domain/UserAccount.java
+++ src/main/java/com/community/domain/UserAccount.java
@@ -31,16 +31,22 @@ public class UserAccount extends AuditingFields {
 
     protected UserAccount() {}
 
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
     }
 
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return UserAccount.of(userId, userPassword, email, nickname, memo, null);
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
 
     @Override
