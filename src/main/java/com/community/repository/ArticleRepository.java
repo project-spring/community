5c4d80e776d7b96650ee456b5552f6b2eb988b6d
CopyPaste <sjnnj@naver.com>
2024-09-19 16:08:39 +0900

    #36 - 도메인에 맞게 repository 를 작성

---
+++ src/main/java/com/community/repository/ArticleRepository.java
@@ -0,0 +1,39 @@
+package com.community.repository;
+
+import com.community.domain.Article;
+import com.community.domain.QArticle;
+import com.community.repository.querydsl.ArticleRepositoryCustom;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        ArticleRepositoryCustom,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+
+    void deleteByIdAndUserAccount_UserId(Long articleId, String userId);
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtags, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);
+        bindings.bind(root.hashtags.any().hashtagName).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+}
