0c75e526bb4d0e4874abf3a12a4c347e36b77935
CopyPaste <sjnnj@naver.com>
2024-09-19 10:51:48 +0900

    #18 - erd 를 참고하여 엔티티 클래스 설계
    - `Article`
    - `ArticleComment`
    - `Hashtag`
    
    엔티티 작성

---
+++ src/main/java/com/community/domain/ArticleComment.java
@@ -0,0 +1,76 @@
+package com.community.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Getter
+@ToString(callSuper = true)
+public class ArticleComment extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article; // 게시글 (ID)
+
+    @Setter
+    @JoinColumn(name = "userId")
+    @ManyToOne(optional = false)
+    private UserAccount userAccount; // 유저 정보 (ID)
+
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId; // 부모 댓글 ID
+
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    @OrderBy("createdAt ASC")
+    @ToString.Exclude
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
+    @Setter @Column(nullable = false, length = 500) private String content; // 본문
+
+
+    protected ArticleComment() {}
+
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
+        this.article = article;
+        this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return this.getId() != null && this.getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
+}
